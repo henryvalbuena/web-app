563246a0cb680ae239a71ecb961cc071f46e0c7e
roycloud <dev.henryvm@gmail.com>
2017-11-09 04:11:19 +0000

    add and delete todo

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express         =       require("express"),
     app             =       express(),
     mongoose        =       require("mongoose"),
-    methodOverride  =   require("method-override");
+    bodyParser      =       require("body-parser"),
+    methodOverride  =       require("method-override");
     
     
 app.set("view engine", "ejs");
@@ -12,10 +13,30 @@ app.use(require("express-session")({
     resave: false,
     saveUninitialized: false
 }));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+var arrTodo = ["Clean the closet", "Make the bed"];
 
 app.get('/', function(req, res){
     res.render('home');
 });
+
+app.get('/todoapp', function(req, res){
+    res.render('todoapp', {arrTodo});
+});
+
+app.post('/todoapp', function(req, res){
+    if(req.body.newTodo){
+    arrTodo.push(req.body.newTodo);
+    }
+    res.redirect('/todoapp');
+});
+app.delete('/todoapp/:index', function(req, res){
+    console.log(req.params.index);
+    arrTodo.splice(req.params.index, 1);
+    res.redirect('/todoapp');
+})
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server started...");
