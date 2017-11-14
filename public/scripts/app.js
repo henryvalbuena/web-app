f7ddff5c2a94991a5fb0e139811be214450e0cf7
roycloud <dev.henryvm@gmail.com>
2017-11-14 13:39:27 +0000

    updated menu bar and size view

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,23 @@
+function checkOnStart() {
+    if( $( window ).width() <= 736){
+      $( "#desktop-m" ).hide();  
+      $( "#mobile-m" ).show(); 
+    } else if($( window ).width() >= 736){
+      $( "#mobile-m" ).hide();
+      $( "#desktop-m" ).show();
+    }
+}
+
+checkOnStart();
+
 $(".show-d").on("click", function(event){
     $(".ui.red.button.delete-b").fadeToggle(500);
+});
+$('.ui.dropdown.icon.item')
+   .dropdown({
+    on: 'hover'
+  })
+;
+$( window ).resize(function() {
+  checkOnStart();
 });
